b277b90c66f65347de561558edf25ce420136148
willwv <william.walder@gmail.com>
2019-03-21 00:07:56 -0300

    Mecanismo de envio de e-mail.

--- TrabalhoFinalJava/src/main/java/com/javaee/willwv/TrabalhoFinalJava/TrabalhoFinalJavaApplication.java
+++ TrabalhoFinalJava/src/main/java/com/javaee/willwv/TrabalhoFinalJava/TrabalhoFinalJavaApplication.java
@@ -3,11 +3,23 @@ package com.javaee.willwv.TrabalhoFinalJava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.javaee.willwv.TrabalhoFinalJava.config.EmailConfig;
+
+
 @SpringBootApplication
 public class TrabalhoFinalJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhoFinalJavaApplication.class, args);
+		final String fromEmail = "mail@gmail.com";
+		final String password = "INSIRA_SENHA_AQUI";
+		final String toEmail = "william.walder@gmail.com";
+		
+		System.out.println("Initializing email send");
+		
+		EmailConfig config = new EmailConfig();
+		
+		config.sendEmail(fromEmail, password, toEmail, "Subject", "Email Body");
 	}
 
 }
---
+++ TrabalhoFinalJava/src/main/java/com/javaee/willwv/TrabalhoFinalJava/config/EmailConfig.java
@@ -0,0 +1,92 @@
+package com.javaee.willwv.TrabalhoFinalJava.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailConfig {
+
+	private Properties getProperties() throws IOException {
+		InputStream inputStream = null;
+		try {
+			Properties properties = new Properties();
+			String propertyFile = "config.properties";
+ 
+			inputStream = getClass().getClassLoader().getResourceAsStream(propertyFile);
+ 
+			if (inputStream != null) {
+				properties.load(inputStream);
+			} else {
+				throw new FileNotFoundException("File '" + propertyFile + "' not found on classpath");
+			}
+
+			return properties;
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+		return null;
+	}
+	
+	public void sendEmail(final String fromEmail,
+						  final String password,
+						  String toEmail,
+						  String subject,
+						  String body){
+		try {
+			Authenticator auth = configAuth(fromEmail, password);
+			
+			Session session = Session.getInstance(getProperties(), auth);
+			
+			MimeMessage message = new MimeMessage(session);
+			configHeaders(message);
+			configContent(toEmail, subject, body, message);
+			System.out.println("Message ready!");
+			
+			Transport.send(message);
+			System.out.println("Email sent successfully!!");
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+	}
+
+	private void configContent(String toEmail, String subject, String body, MimeMessage message) {
+		try {
+			message.setFrom(new InternetAddress("example@gmail.com", "Example"));
+			message.setReplyTo(InternetAddress.parse("example@gmail.com", false));
+			message.setSubject(subject, "UTF-8");
+			message.setText(body, "UTF-8");
+			message.setSentDate(new Date());
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void configHeaders(MimeMessage message) throws MessagingException {
+		message.addHeader("Content-type", "text/HTML; charset=UTF-8");
+		message.addHeader("format", "flowed");
+		message.addHeader("Content-Transfer-Encoding", "8bit");
+	}
+
+	private Authenticator configAuth(final String fromEmail, final String password) {
+		Authenticator auth = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};
+		return auth;
+	}
+}
